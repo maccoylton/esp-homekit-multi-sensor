34f9bd9fb130d4244aac9e57e80da88c428a3204
maccoylton <dave@maccoylton.com>
2018-12-02 21:02:43 +0000

    =various updates

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 /*
  * Example of using esp-homekit library to control
- * moition sensor using an Hs501 sensor
+ * mutiple sensors
  * The use and OTA mechanis created by HomeACcessoryKid 
  *
  */
 
 #define DEVICE_MANUFACTURER "David B Brown"
-#define DEVICE_NAME "Light-Sensor"
-#define DEVICE_MODEL "LM393"
+#define DEVICE_NAME "Muti-Sensor"
+#define DEVICE_MODEL "1"
 #define DEVICE_SERIAL "12345678"
 #define FW_VERSION "1.0"
-
+#define MOTION_SENSOR_GPIO 12
+#define TEMPERATURE_SENSOR_PIN 4
+#define TEMPERATURE_POLL_PERIOD 10000
 
 #include <stdio.h>
 #include <espressif/esp_wifi.h>
@@ -24,7 +26,7 @@
 
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
-
+#include <dht/dht.h>
 
 // add this section to make your device OTA capable
 // create the extra characteristic &ota_trigger, at the end of the primary service (before the NULL)
@@ -39,8 +41,19 @@ homekit_characteristic_t manufacturer     = HOMEKIT_CHARACTERISTIC_(MANUFACTURER
 homekit_characteristic_t serial           = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, DEVICE_SERIAL);
 homekit_characteristic_t model            = HOMEKIT_CHARACTERISTIC_(MODEL,         DEVICE_MODEL);
 homekit_characteristic_t revision         = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION,  FW_VERSION);
+
+//light sensor
 homekit_characteristic_t currentAmbientLightLevel = HOMEKIT_CHARACTERISTIC_(CURRENT_AMBIENT_LIGHT_LEVEL, 0,.min_value = (float[]) {0},);
 homekit_characteristic_t status_active    = HOMEKIT_CHARACTERISTIC_(STATUS_ACTIVE, 1);
+
+//motion sensor
+homekit_characteristic_t motion_detected  = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, 0);
+
+//temperature sensor
+homekit_characteristic_t current_temperature = HOMEKIT_CHARACTERISTIC_( CURRENT_TEMPERATURE, 0 );
+
+//humidity sensor
+homekit_characteristic_t current_relative_humidity    = HOMEKIT_CHARACTERISTIC_( CURRENT_RELATIVE_HUMIDITY, 0 );
 
 
 void identify_task(void *_args) {
@@ -69,6 +82,21 @@ homekit_accessory_t *accessories[] = {
  	    &currentAmbientLightLevel,
 	    &status_active,
             &ota_trigger,
+            NULL
+        }),
+        HOMEKIT_SERVICE(MOTION_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Motion Sensor"),
+            &motion_detected,
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            &current_temperature,
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &current_relative_humidity,
             NULL
         }),
         NULL
@@ -109,6 +137,61 @@ void create_accessory_name() {
     serial.value = name.value;
 }
 
+
+void temperature_sensor_task(void *_args) {
+
+    gpio_set_pullup(TEMPERATURE_SENSOR_PIN, false, false);
+
+    float humidity_value, temperature_value;
+    while (1) {
+        bool success = dht_read_float_data(
+            DHT_TYPE_DHT22, TEMPERATURE_SENSOR_PIN,
+            &humidity_value, &temperature_value
+        );
+
+        if (success) {
+            printf("Got readings: temperature %g, humidity %g\n", temperature_value, humidity_value);
+            current_temperature.value = HOMEKIT_FLOAT(temperature_value);
+            current_relative_humidity.value = HOMEKIT_FLOAT(humidity_value);
+
+            homekit_characteristic_notify(&current_temperature, current_temperature.value);
+            homekit_characteristic_notify(&current_relative_humidity, current_relative_humidity.value);
+
+        } else {
+            printf("Couldnt read data from sensor\n");
+        }
+        vTaskDelay(TEMPERATURE_POLL_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+
+void temperature_sensor_init() {
+    xTaskCreate(temperature_sensor_task, "Temperature", 256, NULL, 2, NULL);
+}
+
+
+void motion_sensor_callback(uint8_t gpio) {
+
+
+    if (gpio == MOTION_SENSOR_GPIO){
+        int new = 0;
+        new = gpio_read(MOTION_SENSOR_GPIO);
+        motion_detected.value = HOMEKIT_BOOL(new);
+        homekit_characteristic_notify(&motion_detected, HOMEKIT_BOOL(new));
+        printf("Motion Detected on %d", gpio);
+    }
+    else {
+        printf("Interrupt on %d", gpio);
+    }
+
+}
+
+void motion_sensor_init() {
+
+    gpio_enable(MOTION_SENSOR_GPIO, GPIO_INPUT);
+    gpio_set_pullup(MOTION_SENSOR_GPIO, false, false);
+    gpio_set_interrupt(MOTION_SENSOR_GPIO, GPIO_INTTYPE_EDGE_ANY, motion_sensor_callback);
+}
+
 void light_sensor_task(void *_args) {
 
 // thaks to https://github.com/peros550/esp-homekit-multiple-sensors/blob/master/examples/multiple_sensors/multiple_sensors.c
@@ -136,7 +219,8 @@ void user_init(void) {
 
     create_accessory_name(); 
     light_sensor_init();
-
+    motion_sensor_init();
+    temperature_sensor_init();
 
     int c_hash=ota_read_sysparam(&manufacturer.value.string_value,&serial.value.string_value,
                                       &model.value.string_value,&revision.value.string_value);
